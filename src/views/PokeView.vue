d388c66d7f71313d992af45a54587c0015517207
lsauquillo <lsauquillo@gmail.com>
2023-05-02 12:06:48 +0200

    ruta_con_parametrod

---
+++ src/views/PokeView.vue
@@ -0,0 +1,35 @@
+<template>
+   <h1>Poke name: {{ $route.params.pokename }}</h1> 
+   <button class="boton" @click="back">Volver</button>
+</template>
+
+<script setup>
+import axios from "axios"
+import { useRoute, useRouter } from "vue-router"
+
+const route = useRoute() //para tener acceso a los params
+const router = useRouter() //para empujar a otra ruta
+
+  const back = ()=>{
+    router.push('/pokemons')
+  }
+
+  const getPoke = async ()=>{
+    try{
+     const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.pokename}`)
+     console.log( data)        
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+  getPoke()
+    
+</script>
+
+<style scoped>
+  .boton{
+    width: 60px;
+  }   
+</style>
---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pokemon">
+    <h1>This is an pokemon page</h1>
+    
+    <ul>
+      <li v-for="(item, index) in pokemons" :key="index ">
+        <RouterLink :to="`/pokemons/${item.name}`">{{item.name}}</RouterLink>        
+      </li>
+    </ul>
+    
+    
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { RouterLink } from 'vue-router';
+
+
+const pokemons = ref([])
+
+const getPokemons = async () => {
+  try {
+    const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    //console.log(data.results);
+    pokemons.value = data.results
+    console.log(pokemons.value)
+  } catch (err) {
+    console.log(err);
+  }
+};
+getPokemons()
+</script>
+
+<style scoped>
+</style>
